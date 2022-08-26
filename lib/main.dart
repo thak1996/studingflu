8670b0c8c70e727139fd4430262c2c8156250185
Franklyn Viana <franklyn.vs@hotmail.com>
2022-08-26 15:18:33 -0300

    Aula 6 - Estados(StatelessWidget)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,27 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(Container(
-    child: const Center(
-      child: Text('Flutterando', textDirection: TextDirection.ltr,
-      style: TextStyle(color: Colors.white, fontSize: 50),
+  runApp(const AppWidget(title: 'Flutterando 3'));
+}
+
+class AppWidget extends StatelessWidget {
+  final String title;
+  const AppWidget({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          title,
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.black, fontSize: 50),
+        ),
       ),
-    ),
-  ));
+    );
+  }
 }
