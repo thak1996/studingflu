488ef59631b4e2f6af875db954bb808f566c25f4
Franklyn Viana <franklyn.vs@hotmail.com>
2022-08-26 15:37:34 -0300

    Aula 10 - a "Cara" do Aplicativo(Scaffold)

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,15 +11,30 @@ class _HomePageState extends State<HomePage> {
   int counter = 0;
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: GestureDetector(
-        child: Text('Contador: $counter'),
-        onTap: () {
-          setState(() {
-            counter++;
-          });
-        },
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
       ),
+      body: Center(
+        child: GestureDetector(
+          child: Text(
+            'Contador: $counter',
+            style: const TextStyle(fontSize: 30),
+          ),
+          onTap: () {
+            setState(() {
+              counter++;
+            });
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              counter++;
+            });
+          }),
     );
   }
 }
