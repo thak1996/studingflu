b790574f6d805081df2c4164bd11d88b8e2f4d05
Franklyn Viana <franklyn.vs@hotmail.com>
2022-08-26 23:32:50 -0300

    Aula 21 - Image.network ( Add image net )

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -23,6 +23,13 @@ class _LoginPageState extends State<LoginPage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Image.network(
+                      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Logo_TV_2015.png/250px-Logo_TV_2015.png'),
+                ),
+                SizedBox(height: 20,),
                 TextField(
                   onChanged: (text) {
                     email = text;
