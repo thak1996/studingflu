a2fac47b5710cda3375f62228c3c63d175108b5e
Franklyn Viana <franklyn.vs@hotmail.com>
2022-08-26 23:44:26 -0300

    Aula 22 - Stack

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -26,8 +26,7 @@ class _LoginPageState extends State<LoginPage> {
                 SizedBox(
                   width: 100,
                   height: 100,
-                  child: Image.network(
-                      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Logo_TV_2015.png/250px-Logo_TV_2015.png'),
+                  child: Image.asset('assets/images/logo.png'),
                 ),
                 SizedBox(height: 20,),
                 TextField(
